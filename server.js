b6e5bf5ad335a25b371e7af867a42041d65c99f8
MarcinDraus <marcin.drauss@gmail.com>
2023-11-18 18:50:46 +0100

    const storage = multer.diskStorage({

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
@@ -5,15 +6,21 @@ const multer = require('multer');
 const app = express();
 const hbs = require('express-handlebars');
 
-// Konfiguracja silnika szablonów Handlebars dla Express
+// Konfiguracja Handlebars
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: path.join(__dirname, 'views/layouts'), defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
-
-// Dodanie middleware do obsługi przesyłania danych z formularzy (dane kodowane jako URL)
 app.use(express.urlencoded({ extended: false }));
 
-// Stworzenie instancji multer z konfiguracją dla przechowywania plików w pamięci
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, 'public/uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+
+// Konfiguracja Multer do przechowywania plików na dysku
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
@@ -21,27 +28,21 @@ const upload = multer({
     const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = allowedFileTypes.test(file.mimetype);
     if (extname && mimetype) {
-      return cb(null, true);
+      cb(null, true);
     } else {
       cb('Error: Only images are allowed!');
     }
   },
 });
 
-// Konfiguracja endpointu dla przesyłania plików w formularzu kontaktowym
+// Obsługa wysyłania plików
 app.post('/contact/send-message', upload.single('projectDesign'), (req, res) => {
-  // Pobranie danych z formularza
   const { author, sender, title, message } = req.body;
 
-  // Sprawdzenie, czy wszystkie wymagane pola są wypełnione
   if (author && sender && title && message) {
-    // Jeśli plik został przesłany, pobierz jego zawartość jako base64
-    const imageBase64 = req.file ? req.file.buffer.toString('base64') : '';
-    
-    // Renderowanie widoku 'contact' z flagą isSent i danymi do wyświetlenia obrazka
-    res.render('contact', { isSent: true, imageBase64: imageBase64 });
+    const fileName = req.file ? req.file.originalname : '';
+    res.render('contact', { isSent: true, fileName: fileName });
   } else {
-    // Renderowanie widoku 'contact' z flagą isError
     res.render('contact', { isError: true });
   }
 });
